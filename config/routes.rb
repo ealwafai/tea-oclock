8170ffa67998732b9fc72fc585cb60d56acc7e87
Ezzedine Alwafai <ezzowafai@gmail.com>
2022-01-11 13:44:05 -0700

    Add initial routes + add factorybot config to rails helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :customers, only: :index do
+        resources :memberships, only: [:index, :create, :update]
+      end
+    end
+  end
 end
